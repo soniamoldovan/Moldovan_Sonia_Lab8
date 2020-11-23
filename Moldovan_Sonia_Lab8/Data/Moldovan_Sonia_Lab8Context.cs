31ef6ffb3fa3316c4868cded4948f8cc47643d80
SONIA-CLAUDIA MOLDOVAN <sonia.claudia.moldovan@stud.ubbcluj.ro>
2020-11-23 19:00:50 +0200

    Add project files.

---
+++ Moldovan_Sonia_Lab8/Data/Moldovan_Sonia_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Sonia_Lab8.Models;
+
+namespace Moldovan_Sonia_Lab8.Data
+{
+    public class Moldovan_Sonia_Lab8Context : DbContext
+    {
+        public Moldovan_Sonia_Lab8Context (DbContextOptions<Moldovan_Sonia_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moldovan_Sonia_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Moldovan_Sonia_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Moldovan_Sonia_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Moldovan_Sonia_Lab8/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Sonia_Lab8.Data;
+using Moldovan_Sonia_Lab8.Models;
+
+namespace Moldovan_Sonia_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Moldovan_Sonia_Lab8.Data.Moldovan_Sonia_Lab8Context _context;
+
+        public IndexModel(Moldovan_Sonia_Lab8.Data.Moldovan_Sonia_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
