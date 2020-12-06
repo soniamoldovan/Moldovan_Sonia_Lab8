ea22d9a9d0373b15f8011d1bd47faf4294e2a1f6
SONIA-CLAUDIA MOLDOVAN <sonia.claudia.moldovan@stud.ubbcluj.ro>
2020-12-06 21:32:56 +0200

    Lab9

--- Moldovan_Sonia_Lab8/Models/Book.cs
+++ Moldovan_Sonia_Lab8/Models/Book.cs
@@ -11,10 +11,13 @@ namespace Moldovan_Sonia_Lab8.Models
     {
         public int ID { get; set; }
 
+        [Required, StringLength(150, MinimumLength = 3)]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume'"), Required, StringLength(50, MinimumLength = 3)]
         public string Author { get; set; }
 
+        [Range(1, 300)]
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
 
